6ca67e820c2768e0e40cbf853a90916bb7fb3af8
Bruna Lais Lindner <brunalaislindner@outlook.com>
2025-09-22 19:56:03 -0300

    Get

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SistemaBiblioteca.Data;
 using SistemaBiblioteca.Models;
@@ -32,6 +33,28 @@ namespace SistemaBiblioteca.Controllers
             await _appDbContext.SaveChangesAsync();
 
             return Created("Livro adicionado: ", livro);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Livro>>> GetLivros()
+        {
+            var livros = await _appDbContext.BIBLIOTECA.ToListAsync();
+
+            return Ok(livros);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Livro>> GetLivro(int id)
+        {
+            var livro = await _appDbContext.BIBLIOTECA.FindAsync(id);
+
+            if (livro == null)
+            {
+                return NotFound("Livro não encontrado!");
+            }
+
+            return Ok(livro);
         }
     }
 }
